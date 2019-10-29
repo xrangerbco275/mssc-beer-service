d609c36b2069462ad659cb4e2c75d7d812c85310
joe cruz <xrangerbco275@att.net>
2019-10-29 16:12:12 -0500

    initial commit. Closes issue #1

---
+++ src/test/java/guru/springframework/msscbeeerservice/MsscBeeerServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeeerServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
