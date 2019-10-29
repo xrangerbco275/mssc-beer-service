67fff8c9bb287c0c6598b38ad59c83095fcbc423
joe cruz <xrangerbco275@att.net>
2019-10-29 16:28:49 -0500

    Create Data Model #2

---
+++ src/main/java/guru/springframework/msscbeeerservice/web/model/BeerDto.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscbeeerservice.web.model;
+
+import guru.springframework.msscbeeerservice.web.model.enums.BeerStyleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class BeerDto
+{
+
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+
+}
