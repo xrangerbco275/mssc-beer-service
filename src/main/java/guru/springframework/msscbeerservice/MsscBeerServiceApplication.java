35cdc7c0624de37f7f948441ed737c82ca204389
xrangerbco275 <xrangerbco275@att.net>
2019-11-09 20:10:55 -0600

    Code clean up

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsscBeeerServiceApplication
+public class MsscBeerServiceApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(MsscBeeerServiceApplication.class, args);
+        SpringApplication.run(MsscBeerServiceApplication.class, args);
     }
 
 }
